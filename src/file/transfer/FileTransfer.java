0afe467808e91231a0d6c751816909a68c2e1bbd
kaushal28 <shah.kaushal95@gmail.com>
2016-12-17 16:27:37 +0530

    gui

--- src/file/transfer/FileTransfer.java
+++ src/file/transfer/FileTransfer.java
@@ -5,9 +5,12 @@
  */
 package file.transfer;
 
+import java.awt.Dimension;
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 /**
  *
@@ -26,6 +29,23 @@ public class FileTransfer {
         
         Scanner in = new Scanner(System.in);
         int choise = in.nextInt();
+        
+        
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run() {
+                JFrame frame = new JFrame();
+                frame.add(new MainPanel());
+                frame.setSize(new Dimension(700,700));
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+                
+                
+                
+            }
+             
+        });
+        
 
         switch(choise){
             case 1:
